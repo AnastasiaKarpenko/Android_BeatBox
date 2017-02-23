418a6388f770ef5b7289b5e5f724b7e6413aa9ea
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2017-02-23 10:25:33 +0200

    inflating a binding class

--- app/src/main/java/ws/tilda/anastasia/beatbox/BeatBoxFragment.java
+++ app/src/main/java/ws/tilda/anastasia/beatbox/BeatBoxFragment.java
@@ -1,7 +1,15 @@
 package ws.tilda.anastasia.beatbox;
 
 
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ws.tilda.anastasia.beatbox.databinding.FragmentBeatBoxBinding;
 
 public class BeatBoxFragment extends Fragment {
 
@@ -9,7 +17,13 @@ public class BeatBoxFragment extends Fragment {
         return new BeatBoxFragment();
     }
 
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        FragmentBeatBoxBinding binding = DataBindingUtil
+                .inflate(inflater, R.layout.fragment_beat_box, container, false);
+        return binding.getRoot();
 
-
-
+    }
 }
