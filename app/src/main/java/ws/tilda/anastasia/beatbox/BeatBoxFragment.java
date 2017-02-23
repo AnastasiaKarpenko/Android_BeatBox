94b967edea054c28ef07681b3279ced006703da9
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2017-02-23 10:09:14 +0200

    Initial commit

---
+++ app/src/main/java/ws/tilda/anastasia/beatbox/BeatBoxFragment.java
@@ -0,0 +1,15 @@
+package ws.tilda.anastasia.beatbox;
+
+
+import android.support.v4.app.Fragment;
+
+public class BeatBoxFragment extends Fragment {
+
+    public static BeatBoxFragment newInstance() {
+        return new BeatBoxFragment();
+    }
+
+
+
+
+}
