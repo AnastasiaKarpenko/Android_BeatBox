45cec4aa7d5790b72e0f9bd731f70c48432e4f2f
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2017-02-24 11:07:43 +0200

    creating soundPool and loading sounds

--- app/src/main/java/ws/tilda/anastasia/beatbox/BeatBox.java
+++ app/src/main/java/ws/tilda/anastasia/beatbox/BeatBox.java
@@ -1,23 +1,32 @@
 package ws.tilda.anastasia.beatbox;
 
 import android.content.Context;
+import android.content.res.AssetFileDescriptor;
 import android.content.res.AssetManager;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.util.Log;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.google.common.base.Ascii.MAX;
+
 public class BeatBox {
     public static final String TAG = "BeatBox";
 
     public static final String SOUNDS_FOLDER = "sample_sounds";
+    public static final int MAX_SOUNDS = 5;
 
     private AssetManager mAssets;
     private List<Sound> mSounds = new ArrayList<>();
+    private SoundPool mSoundPool;
 
     public BeatBox(Context context) {
         mAssets = context.getAssets();
+        // This old constructor is deprecated but needed for compatibility with API 19
+        mSoundPool = new SoundPool(MAX_SOUNDS, AudioManager.STREAM_MUSIC, 0);
         loadSounds();
     }
 
@@ -32,10 +41,21 @@ public class BeatBox {
         }
 
         for (String filename : soundNames) {
-            String assetPath = SOUNDS_FOLDER + "/" + filename;
-            Sound sound = new Sound(assetPath);
-            mSounds.add(sound);
+            try {
+                String assetPath = SOUNDS_FOLDER + "/" + filename;
+                Sound sound = new Sound(assetPath);
+                load(sound);
+                mSounds.add(sound);
+            } catch (IOException ioe) {
+                Log.e(TAG, "Coundl not load sound" + filename, ioe);
+            }
         }
+    }
+
+    private void load(Sound sound) throws IOException {
+        AssetFileDescriptor afd = mAssets.openFd(sound.getAssetPath());
+        int soundId = mSoundPool.load(afd, 1);
+        sound.setSoundId(soundId);
     }
 
     public List<Sound> getSounds() {
